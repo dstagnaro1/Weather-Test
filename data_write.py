f5880dbb140599385712b49c58d7ee6d6b69f7a2
Daniel Stagnaro <dstagnaro@gmail.com>
2020-02-08 20:11:43 -0800

    Update data_write.py
    updated comments and description of .py file

--- data_write.py
+++ data_write.py
@@ -1,3 +1,18 @@
+# Generates a time stamp, and humidity percentage.
+# day stamp is noted with a single digit indicating day of year
+# its easier than writing a day and month formatted, and easier to reprocess
+# file is attempt to write as little data as possible to file
+# Data is written Hour Colon Minute Dash Humidity
+# h:m:-h
+# as simple as possible of course
+# humidity is primary focus, temperature second.
+# the two datas will be written to seperate files.
+# when recording real data, humidity will be recorded on the 15 second mark,
+# and temperature on the 45 second mark per minute
+# averages will be reached, but the question im trying to solve is how much data is enough
+# 1 minute seems good
+
+
 from random import randint as ri
 DAYS_IN_YEAR = 365 # just print out day of year
 HOURS_IN_DAY = 24
@@ -8,8 +23,10 @@ min_humidity = 1
 max_humidity = 100
 
 def update_humidity(current):
+	# eventually this data will be generated from an actual sensor
 	new = ri(current - 5, current + 5)
 
+	# keeps humidity between range of low and high
 	if new < min_humidity:
 		new = min_humidity
 	elif new > max_humidity:
@@ -18,11 +35,11 @@ def update_humidity(current):
 
 data_file = open("humidity_data.txt","a+")
 
-
+# generate a starting humidity
 humidity = update_humidity(starting_humidity)
 
 for d in range(DAYS_IN_YEAR):
-	# print(d)
+
 	data_file.write(f"{d}\n")
 
 	for h in range(HOURS_IN_DAY):
@@ -31,6 +48,7 @@ for d in range(DAYS_IN_YEAR):
 			# print(f"{h}:{m}-{humidity}")
 			data_file.write(f"{h}:{m}-{humidity}\n")
 
+			# get a new humidity
 			humidity = update_humidity(humidity)
 
 data_file.close()
